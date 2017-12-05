6fcead987370428a4fcaf3833c5f144f1b9ebc0a
Auriane de Premilhat <adeprem@vt.edu>
2017-12-05 22:49:37 +0100

    added php form but not sure it works. modified colors of buttons and links on hover

---
+++ mailer.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_POST['submit'])) {
+
+$to = "adeprem@vt.edu";
+$subject = "Contact";
+$name_field = $_POST['name'];
+$email_field = $_POST['email'];
+$message = $_POST['message'];
+
+$body = "From: $name_field\n E-Mail: $email_field\n Message:\n $message";
+
+echo "Merci de nous avoir contactés!";
+mail($to, $subject, $body);
+
+} else {
+
+echo "Erreur";
+
+}
+?>
