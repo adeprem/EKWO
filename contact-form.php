52fe6c39da2f861628a72efa0fb7e71b54be2ffa
Auriane de Premilhat <31307153+adeprem@users.noreply.github.com>
2019-01-16 22:54:32 +0100

    Add files via upload

--- contact-form.php
+++ contact-form.php
@@ -7,14 +7,16 @@
     $to = 'adeprem@vt.edu';
     $subject = 'Formulaire de contact';
 
-    $body = 'From: $name"\n" E-Mail: $email "\n" Message: "\n" $message';
+    // $body = "From: $name E-Mail: $email Message: $message";
+    $body = 'From: '.$name."\r\n".'E-mail: '.$email."\r\n".'Message'.$message;
+
 
     if ($_POST['submit']) {
       if (mail($to, $subject, $body, $from)) {
         echo '<p>Your message has been sent!</p>';
       }
       else {
-        echo '<p>Désolée, il y a eu un problème. Si cela persiste, contactez nous par email ! info@dgpourpre.fr !</p>';
+        echo '<p>Désolée, il y a eu un problème. Si cela persiste, contactez nous par email ! info@dgpourpre.fr</p>';
       }
     }
 ?>
