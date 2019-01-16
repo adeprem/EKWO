4a5e49e0b5b592cfaa427e453c9bb26cd0320eb9
Auriane de Premilhat <31307153+adeprem@users.noreply.github.com>
2019-01-16 22:36:16 +0100

    Add files via upload

--- contact-form.php
+++ contact-form.php
@@ -1,33 +1,20 @@
-    <?php
-    // S'il y des données de postées
-    if ($_SERVER['REQUEST_METHOD']=='POST') {
 
-      // (1) Code PHP pour traiter l'envoi de l'email
+<?php
+    $name = $_POST['nom'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: Site Ekwo';
+    $to = 'adeprem@vt.edu';
+    $subject = 'Formulaire de contact';
 
-      // Récupération des variables et sécurisation des données
-      $nom     = htmlentities($_POST['nom']); // htmlentities() convertit des caractères "spéciaux" en équivalent HTML
-      $email   = htmlentities($_POST['email']);
-      $message = htmlentities($_POST['message']);
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-      // Variables concernant l'email
-
-      $destinataire = 'adeprem@vt.edu'; // Adresse email du webmaster (à personnaliser)
-      $sujet = 'Titre du message'; // Titre de l'email
-      $contenu = '<html><head><title>Titre du message</title></head><body>';
-      $contenu .= '<p>Bonjour, vous avez reçu un message à partir de votre site web.</p>';
-      $contenu .= '<p><strong>Nom</strong>: '.$nom.'</p>';
-      $contenu .= '<p><strong>Email</strong>: '.$email.'</p>';
-      $contenu .= '<p><strong>Message</strong>: '.$message.'</p>';
-      $contenu .= '</body></html>'; // Contenu du message de l'email (en XHTML)
-
-      // Pour envoyer un email HTML, l'en-tête Content-type doit être défini
-      $headers = 'MIME-Version: 1.0'."\r\n";
-      $headers .= 'Content-type: text/html; charset=utf-8'."\r\n";
-      header('Content-Type: text/html; charset=utf-8');
-
-      // Envoyer l'email
-      mail($destinataire, $sujet, $contenu, $headers); // Fonction principale qui envoie l'email
-      echo '<h2>Message envoyé!</h2>'; // Afficher un message pour indiquer que le message a été envoyé
-      // (2) Fin du code pour traiter l'envoi de l'email
+    if ($_POST['submit']) {
+      if (mail ($to, $subject, $body, $from)) {
+        echo '<p>Your message has been sent!</p>';
+      }
+      else {
+        echo '<p>Désolée, il y a eu un problème. Si cela persiste, contactez nous par email ! !</p>';
+      }
     }
-    ?>
+?>
